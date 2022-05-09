0c15604a87b816d9dc5b753048e72479a3c46380
Aaron Monterroso <student48@Aaron-M.local>
2022-05-09 16:12:19 -0400

    adding files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import HeroBanner from './components/HeroBanner';
-import Navbar from './components/Navbar';
-import Card from './components/Card';
+import {Routes, Route, Link} from 'react-router-dom';
+
+//import HeroBanner from './components/HeroBanner';
+//import Navbar from './components/Navbar';
+// import Card from './components/Card';
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Profile from './Pages/Profile'
+import Dropdown from './components/Dropdown.js'
+import Footer from './components/Footer.js'
+import Card from './components/Card.js'
 function App() {
   return (
     <div className="App">
-      <HeroBanner/>
-      <Navbar/>
+
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/profile'>Profile</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+      <Dropdown />
       <Card/>
+      <Footer />
     </div>
   );
 }
---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroBanner from '../components/HeroBanner'
+
+function Home() {
+  return (
+    <div> 
+        Home
+        <HeroBanner />
+
+    </div>
+  )
+}
+
+export default Home
